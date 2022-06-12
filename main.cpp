a517604ab42cce4447accbc344a41faca4e230da
masakado <sponge.cake@tuta.io>
2022-06-12 11:15:24 -0300

    Major changes

--- main.cpp
+++ main.cpp
@@ -1,1 +1,66 @@
 #include "makai.hpp"
+#include <stdexcept>
+
+void Makai::Program::init(unsigned int width, unsigned int height, std::string windowTitle = "WINDOW"){
+	// Initialize GLFW
+	glfwInit();
+	// Set window title to given title
+	this->windowTitle = windowTitle;
+	// Try and create window
+	window = glfwCreateWindow(width, height, windowTitle.c_str(), NULL, NULL);
+	// If window couldn't be created, throw error
+	if (window == NULL)
+		throw std::runtime_error("Window could not be created.");
+	// Show window
+	glfwMakeContextCurrent(window);
+}
+
+void Makai::Program::run() {
+	// The delta (time between frames)
+	float delta = 0.0f;
+	// The last frame's time
+	float last = 0.0f;
+	// While program is running...
+	while(running()) {
+		// Get current time
+		float current = glfwGetTime();
+		// Calculate delta
+		delta = current - last;
+		// Set last frame to current
+		last = current;
+		// Start threads
+		std::thread objects(EntityClass::$_ROOT.yield, EntityClass::$_ROOT, delta);
+		std::thread counters(
+			[&]()->void {
+					Tween::yieldAllTweens();
+					Event::yieldAllTimers();
+
+				}
+			);
+		// Do your own stuff
+		onFrame(delta);
+		// Wait for threads to be done processing
+		objects.join();
+		counters.join();
+	}
+	// Terminate program
+	terminate();
+}
+
+void Makai::Program::close() {
+	// Set execution to end
+	forceStop = true;
+}
+
+void Makai::Program::terminate() {
+	// Force execution end
+	forceStop = true;
+	// End GLFW
+	glfwTerminate();
+	// Close program
+	exit(0);
+}
+
+inline bool Makai::Program::running() {
+	return (glfwWindowShouldClose(window) && !forceStop);
+}
--- makai.hpp
+++ makai.hpp
@@ -8,11 +8,13 @@
 
 #include <string>
 #include <vector>
+#include <functional>
 #include <cstring>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 #include <math.h>
+#include <thread>
 
 /*
 ***************************
@@ -26,12 +28,13 @@
 #define float double
 #endif // _$_FLOAT_AS_DOUBLE
 
-#include <collection/entity/entity.hpp>
-#include <collection/vectorn.hpp>
-#include <collection/cyclical.hpp>
-#include <collection/algebra.hpp>
-#include <collection/matrices.hpp>
-#include <collection/event.hpp>
+#include <entity/entity.hpp>
+#include <vectorn.hpp>
+#include <cyclical.hpp>
+#include <algebra.hpp>
+#include <matrices.hpp>
+#include <tween.hpp>
+#include <event.hpp>
 
 #ifdef _$_FLOAT_AS_DOUBLE
 #undef float
@@ -45,8 +48,8 @@
 *********************
 */
 
-#include <include/Ogre.h>
-#include <include/GLFW/glfw3.h>
+#include <Ogre.h>
+#include <GLFW/glfw3.h>
 
 /*
 #define IF		if (
@@ -59,3 +62,37 @@
 #define EXECUTE	{
 //*/
 
+#define $main [&]() -> void
+
+namespace Makai {
+	namespace {
+		using
+		std::vector,
+		std::function,
+		std::string;
+	}
+	/**
+	***********************
+	*                     *
+	*  Program structure  *
+	*                     *
+	***********************
+	*/
+
+	typedef function<void(float)>	OnFrameFunc;
+
+	struct Program {
+	public:
+		void init(unsigned int, unsigned int, string);
+		void run();
+		void close();
+		OnFrameFunc		onFrame;
+		Event::Signal	onClose;
+		inline bool running();
+	private:
+		void terminate();
+		string windowTitle;
+		GLFWwindow* window;
+		bool forceStop = false;
+	} $_PROGRAM;
+}
