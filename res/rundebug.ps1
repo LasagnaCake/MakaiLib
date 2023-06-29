778330f7d4ba632acf5cfefa35304d225d3ab456
LasagnaCake <sponge.cake@tuta.io>
2023-06-29 15:09:57 -0300

    .

--- res/rundebug.ps1
+++
@@ -1,3 +0,0 @@
-. "$($PSScriptRoot)\debugapp.ps1"
-
-Debug-Program -ProgramName ".\program_debug.exe"
--- res/runrelease.ps1
+++
@@ -1,3 +0,0 @@
-. "$($PSScriptRoot)\debugapp.ps1"
-
-Debug-Program -ProgramName ".\program_release.exe"
