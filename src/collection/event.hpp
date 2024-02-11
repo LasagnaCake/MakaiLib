4d18ebcfbeb79cbd3ffedab4ba096be4597b7246
LasagnaCake <sponge.cake@tuta.io>
2024-02-10 21:15:03 -0300

    Update event.hpp

--- src/collection/event.hpp
+++ src/collection/event.hpp
@@ -220,6 +220,141 @@ namespace Event{
 		/// The current yield cycle.
 		float counter = 0;
 	};
+
+	class Notifier {
+	public:
+		typedef Signal						SignalType;
+
+		struct SignalWrapper {
+			SignalWrapper(SignalType const& s): signal(s), id(count++) {}
+
+			void operator()() const {signal();}
+
+			bool operator==(SignalWrapper const& other) const {return id == other.id;}
+
+			Helper::PartialOrder operator<=>(SignalWrapper const& other) const {return id <=> other.id;}
+		private:
+			SignalType	signal;
+			size_t		id = 0;
+			inline static size_t count = 0;
+		};
+
+		typedef List<SignalType>				SignalList;
+		typedef Arguments<SignalType>			SignalArguments;
+		typedef Dictionary<List<SignalWrapper>>	SignalDatabase;
+
+		Notifier() {}
+
+		Notifier(String const& signal, Signal const& action)				{subscribe(signal, action);		}
+		Notifier(String const& signal, SignalList const& actions)			{subscribe(signal, actions);	}
+		Notifier(String const& signal, SignalArguments const& actions)		{subscribe(signal, actions);	}
+
+		Notifier(Dictionary<SignalList> const& signals)						{subscribe(signals);			}
+		Notifier(Arguments<Pair<String, SignalList>> const& signals)		{subscribe(signals);			}
+		Notifier(Arguments<Pair<String, SignalArguments>> const& signals)	{subscribe(signals);			}
+
+		~Notifier() {
+			unsubscribeFromAll();
+		}
+
+		Notifier& subscribe(String const& signal, Signal const& action) {
+			added[signal].push_back(action);
+			db[signal].push_back(action);
+			return *this;
+		}
+
+		Notifier& subscribe(String const& signal, SignalList const& actions) {
+			for (Signal const& a: actions) {
+				added[signal].push_back(a);
+				db[signal].push_back(a);
+			}
+			return *this;
+		}
+
+		Notifier& subscribe(String const& signal, SignalArguments const& actions) {
+			for (Signal const& a: actions) {
+				added[signal].push_back(a);
+				db[signal].push_back(a);
+			}
+			return *this;
+		}
+
+		Notifier& subscribe(Dictionary<SignalList> const& signals) {
+			for (auto& [name, actions]: signals)
+				subscribe(name, actions);
+			return *this;
+		}
+
+		Notifier& subscribe(Arguments<Pair<String, SignalList>> const& signals) {
+			for (auto& [name, actions]: signals)
+				subscribe(name, actions);
+			return *this;
+		}
+
+		Notifier& subscribe(Arguments<Pair<String, SignalArguments>> const& signals) {
+			for (auto& [name, actions]: signals)
+				subscribe(name, actions);
+			return *this;
+		}
+
+		Notifier& unsubscribe(String const& signal) {
+			if (db.contains(signal)) {
+				auto& al = added[signal];
+				std::erase_if(
+					db[signal],
+					[&] (SignalWrapper& elem) {
+						return std::find(al.begin(), al.end(), elem) != al.end();
+					}
+				);
+			}
+			added[signal].clear();
+			return *this;
+		}
+
+		Notifier& unsubscribe(StringList const& signals) {
+			for (String const& s: signals)
+				unsubscribe(s);
+			return *this;
+		}
+
+		Notifier& unsubscribe(StringArguments const& signals) {
+			for (String const& s: signals)
+				unsubscribe(s);
+			return *this;
+		}
+
+		template <typename... Args>
+		Notifier& unsubscribe(Args const&... signals)
+		requires (... && Type::Equal<Args, String>) {
+			(..., unsubscribe(signals));
+			return *this;
+		}
+
+		Notifier& unsubscribeFromAll() {
+			for (auto& [name, lst]: db) {
+				auto& al = added[name];
+				std::erase_if(
+					lst,
+					[&] (SignalWrapper& elem) {
+						return std::find(al.begin(), al.end(), elem) != al.end();
+					}
+				);
+			}
+			added.clear();
+			return *this;
+		}
+
+		static void broadcast(String const& signal) {
+			if (db.contains(signal))
+				for (SignalWrapper& s: db[signal])
+					s();
+		}
+
+	private:
+		SignalDatabase added;
+
+		inline static SignalDatabase db;
+	};
 }
 
 namespace TypedEvent {
