b2f22931497091d6d56375ba7317693e90bae138
LasagnaCake <sponge.cake@tuta.io>
2024-02-16 12:07:22 -0300

    Scrap that

--- src/interpreter.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MAKAILIB_SCRIPT_INTERPRETER_H
-#define MAKAILIB_SCRIPT_INTERPRETER_H
-
-#include "anchors.hpp"
-
-#include "interpreter/gl_interpreter.hpp"
-
-#endif // MAKAI_SCRIPT_INTERPRETER_H
