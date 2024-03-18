745d217f7d7905efc35596710ab30db0b6cbb784
LasagnaCake <sponge.cake@tuta.io>
2024-03-18 15:37:34 -0300

    Update map.hpp

--- src/collection/types/map.hpp
+++ src/collection/types/map.hpp
@@ -6,8 +6,13 @@
 template<class K, class V, Type::Integer I = size_t>
 struct Map {
 public:
-	typedef K KeyType;
-	typedef V ValueType;
+	typedef K							KeyType;
+	typedef V							ValueType;
+	typedef Pair<KeyType, ValueType>	PairType;
+
+	constexpr Map() {}
+
+private:
 };
 
 #endif // TYPE_MAP_H
