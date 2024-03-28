e7a770c705cb07a45d2f46259be0532e19bef29b
LasagnaCake <sponge.cake@tuta.io>
2024-03-28 16:23:30 -0300

    Update aton.hpp

--- src/collection/ctl/algorithm/aton.hpp
+++ src/collection/ctl/algorithm/aton.hpp
@@ -74,77 +74,106 @@ namespace _AtoiImpl {
 }
 
 template<Type::Integer I, CharacterType T>
-constexpr I atoi(T const* const& str, uintmax size) {
+constexpr bool atoi(T const* const& str, uintmax size, I& out) {
+	// Copy string pointer
 	T const* s = str;
+	// If string is size 1, try and convert digit
 	if (size == 1) {
 		if (isDigitInBase(str[0], 10)) return toDigit(str[0]);
-		else throw "ERROR: Invalid number!";
+		else return false;
 	}
+	// If size is 2, first character is a sign, and is in base 10, convert number to digit
 	if (
 		size == 2
 	&&	_AtoiImpl::isSign(str[0])
 	&&	isDigitInBase(str[1], 10)
-	) return toDigit(str[0]);
+	) return getSignAndConsume(s) * toDigit(s[0]);
+	// Try and get sign of number
 	intmax sign = _AtoiImpl::getSignAndConsume(s);
-	if (size < s-str) throw "ERROR: Invalid number!";
+	if (size < s-str) return false;
+	// Try and get base of number
 	intmax base = _AtoiImpl::getBaseAndConsume(s);
-	if (size < s-str) throw "ERROR: Invalid number!";
+	if (size < s-str) return false;
+	// Remove difference from size
 	size -= s-str;
+	// Check if number is in its intended base
 	if (!_AtoiImpl::isInBase(s, size, base))
-		throw "ERROR: Integer value does not match its base!";
-	intmax val = _AtoiImpl::toInteger<I>(s, size, base);
-	return sign * val;
+		return false;
+	// Convert number to int and return
+	out = sign * _AtoiImpl::toInteger<I>(s, size, base);
+	return true;
 }
 
-template<Type::Integer I, CharacterType T, uintmax N>
-constexpr I atoi(const T(const& str)[N]) {
-	static_assert(N > 1, "String cannot be empty!");
-	return atoi<I>(str, N - 1);
+template<Type::Integer I, CharacterType T, uintmax S>
+constexpr bool atoi(const T(const& str)[S], I& out) {
+	static_assert(S, "String cannot be empty!");
+	return atoi<I>(str, S - 1, out);
 }
 
 template<Type::Real F, CharacterType T>
-constexpr F atof(T const* const& str, uintmax size) {
-	uintmax sep = 0;
+constexpr bool atof(T const* const& str, uintmax size, F& out) {
+	// If character is appended to the end, exclude it
 	if (
 		toLower(str[size-1]) == 'f'
 	||	toLower(str[size-1]) == 'd'
 	) --size;
+	// Find separator character
+	uintmax sep = 0;
 	while (sep < size)
 		if (str[sep++] == '.')
 			break;
-	if (sep == size)
-		return atoi<uintmax>(str, size);
+	// If no separator was found, convert number and return
+	intmax ival = 0;
+	if (sep == size) {
+		if (!atoi<intmax>(str, size, ival))
+			return false;
+		out = ival;
+		return true;
+	}
+	// Create new string without separator
 	T* ns = new T[size-1];
 	memcpy(ns, str, sep-1);
 	memcpy(ns+sep-1, str+sep, size-sep);
-	intmax ival = atoi<uintmax>(ns, size-1);
+	// Try and convert resulting integer string
+	if (!atoi<uintmax>(ns, size-1, ival))
+		return false;
 	delete[] ns;
-	if constexpr (Type::Equal<F, float>)
-		return ival * __builtin_powf(10, -intmax(size-sep));
-	else if constexpr (Type::Equal<F, double>)
-		return ival * __builtin_pow(10, -intmax(size-sep));
-	else
-		return ival * __builtin_powd(10, -intmax(size-sep));
+	// Convert integer to string by "reverse scientific notation" and return
+	out = ival * pow<F>(10, -intmax(size-sep));
+	return true;
+}
+
+template<Type::Real F, CharacterType T, uintmax S>
+constexpr bool atof(const T(const& str)[S], F& out) {
+	static_assert(S, "String cannot be empty!");
+	return atof<F>(str, S - 1, out);
 }
 
 // Based off of https://stackoverflow.com/a/3987783
 template<Type::Integer I, CharacterType T>
-constexpr void itoa(I val, I const& base, T* const& buf, uintmax const& bufSize){
-	if (bufSize < 4) throw "ERROR: Buffer is too small!";
+constexpr intmax itoa(I val, T* const& buf, uintmax const& bufSize, I const& base = 10){
+	// If empty buffer, or buffer is too small for a non-decimal base
+	if ((!bufSize) || (bufSize < 4 && base != 10))
+		return -1;
+	// Clear buffer
 	memset(buf, 0, bufSize);
+	// Get stating points
 	uintmax
 		offset = 0,
 		i = bufSize-2
 	;
+	// If value is 0, set buffer to zero and return
 	if (!val) {
 		buf[0] = '0';
-		return;
+		return 0;
 	}
+	// If value is negative, append negative sign and invert value
 	if (val < 0) {
 		buf[offset] = '-';
 		val = -val;
 		++offset;
 	}
+	// If not decimal, append base identifier accoordingly
 	if (base != 10) {
 		buf[offset] = '0';
 		++offset;
@@ -157,9 +186,34 @@ constexpr void itoa(I val, I const& base, T* const& buf, uintmax const& bufSize)
 			case 8:		break;
 		}
 	}
+	// Calculate number value
 	for(; val && (i-offset); --i, val /= base)
 		buf[i] = "0123456789abcdef"[val % base];
-	memcpy(buf+offset, buf+offset+i, bufSize-i-offset);
+	// Move stuff around to beginning of buffer
+	memcpy(buf+offset, buf+offset+i, bufSize-i);
+	// Return full size of number string
+	return offset+i+1;
+}
+
+template<Type::Real F, CharacterType T>
+constexpr intmax ftoa(F val, T* const& buf, uintmax const& bufSize, uintmax const& precision = sizeof(F)*4) {
+	// Get amount of zeroes to add to number
+	uintmax zeroes = pow<F>(10, precision);
+	// Get whole part of number
+	intmax whole = val;
+	// Get fractional part
+	uintmax frac = (val * zeroes) - (whole * zeroes);
+	intmax lhs = 0, rhs = 0;
+	// Fill in whole part of number to string, return if error
+	if (lhs = itoa<intmax>(whole, buf, bufSize)) return -1;
+	// Check if buffer is not full, else append comma and re-check
+	if (lhs >= bufSize) return lhs;
+	buf[lhs++] = '.';
+	if (lhs >= bufSize) return lhs;
+	// Fill in fractional part, returning if error
+	if (rhs = itoa<intmax>(frac, buf+lhs, bufSize-lhs)) return -1;
+	// Return full size of number string
+	return lhs+rhs+1;
 }
 
 #endif // CTL_ALGORITHM_ATOI_H
