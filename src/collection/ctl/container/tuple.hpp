27b2d866370fa34462d85deb3c6ceb912618f4d8
LasagnaCake <sponge.cake@tuta.io>
2024-04-10 17:06:06 -0300

    Update tuple.hpp

--- src/collection/ctl/container/tuple.hpp
+++ src/collection/ctl/container/tuple.hpp
@@ -14,8 +14,6 @@ struct TuplePack;
 template<usize N>
 struct TuplePack<N>{};
 
-
-
 template<usize N, class T, class... Types>
 struct TuplePack<N, T, Types...>:
 	public TupleLeaf<N, T>,
@@ -28,14 +26,14 @@ struct TuplePack<N, T, Types...>:
 		return get<INDEX>(*this);
 	}
 
-	template<usize N1, class T1, typename... Types1>
+	template<usize N1, class T1, class... Types1>
 	constexpr static T1& get(TuplePack<N1, T1, Types1...>& tup)
 	requires (N1 < TuplePack<N1, T1, Types1...>::DataTypes::COUNT) {
 		return tup.TupleItem<N1, T1>::value;
 	}
 };
 
-template<usize N, class T, typename... Types>
+template<usize N, class T, class... Types>
 T& get(TuplePack<N, T, Types...>& tup)
 requires (N < TuplePack<N, T, Types...>::DataTypes::COUNT) {
     return tup.TupleItem<N, T>::value;
