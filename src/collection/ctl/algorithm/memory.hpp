a6ad74393e40c7fc3cf59b0d5495818cb3e0d2e9
LasagnaCake <sponge.cake@tuta.io>
2024-04-13 21:28:50 -0300

    Update memory.hpp

--- src/collection/ctl/algorithm/memory.hpp
+++ src/collection/ctl/algorithm/memory.hpp
@@ -11,19 +11,29 @@
 
 #include "ctypes.h"
 
-constexpr void memcpy(void* const& src, void* const& dst, usize size) {
-	uchar* s = src, d = dst;
+template<Type::Integer I>
+constexpr void memcpyX(void* const& dst, void* const& src, usize size) {
+	I *s = src, *d = dst;
 	while (size--) *d++ = *s++;
+};
+
+constexpr void memcpy(void* const& dst, void* const& src, usize const& size) {
+	memcpyX<uint8>(dst, src, size);
 };
 
 template<Type::NonVoid T>
 constexpr void memcpy(T* const& src, T* const& dst, usize const& count) {
-	memcpy((void*)src, (void*)dst, count * sizeof(T));
+	const usize sz = count * sizeof(T);
+	if (sz%8==0) return memcpyX<uint64>(dst, src, sz);
+	if (sz%4==0) return memcpyX<uint32>(dst, src, sz);
+	if (sz%2==0) return memcpyX<uint16>(dst, src, sz);
+	return memcpyX<uint8>(dst, src, sz);
 };
 
-constexpr void* memmove(void* const& src, const void* const& dst, usize size) {
-	uchar* d = dest;
-	const uchar* s = src;
+template<Type::Integer I>
+constexpr void* memmoveX(void* const& dst, const void* const& src, usize size) {
+	I* d = dest;
+	const I* s = src;
 	if (d < s)
 		while (size--)
 			*d++ = *s++;
@@ -36,14 +46,23 @@ constexpr void* memmove(void* const& src, const void* const& dst, usize size) {
 	return dst;
 }
 
-template<Type::NonVoid T>
-constexpr T* memmove(T* const& src, const T* const& dst, usize const& size) {
-	return memmove((void*)src, (void*)dst, size * sizeof(T));
+constexpr void* memmove(void* const& dst, const void* const& src, usize size) {
+	return memmoveX<uint8>(dst, src, size);
 }
 
-constexpr int memcmp(const void* const& a, const void* const& b, usize size) {
-	const uchar* s1 = a;
-	const uchar* s2 = b;
+template<Type::NonVoid T>
+constexpr T* memmove(T* const& dst, const T* const& src, usize const& count) {
+	const usize sz = count * sizeof(T);
+	if (sz%8==0) return memmoveX<uint64>((void*)dst, (void*)src, sz);
+	if (sz%4==0) return memmoveX<uint32>((void*)dst, (void*)src, sz);
+	if (sz%2==0) return memmoveX<uint16>((void*)dst, (void*)src, sz);
+	return memmoveX<uint8>((void*)dst, (void*)src, sz);
+}
+
+template<Type::Integer I>
+constexpr int memcmpX(const void* const& a, const void* const& b, usize size) {
+	const I* s1 = a;
+	const I* s2 = b;
 	while (count-- > 0) {
 		if (*s1++ != *s2++)
 		return s1[-1] < s2[-1] ? -1 : 1;
@@ -51,21 +70,34 @@ constexpr int memcmp(const void* const& a, const void* const& b, usize size) {
 	return 0;
 }
 
-template<Type::NonVoid T>
-constexpr int memcmp(T* const& a, T* const& b, usize const& size) {
-	return memcmp((void*)a, (void*)b, size * sizeof(T));
+constexpr int memcmp(const void* const& a, const void* const& b, usize size) {
+	return memcmpX<uint8>(a, b, size);
 }
 
-constexpr void* memset(void* const& dst, int const& val, usize size) {
-	uchar* d = dst;
+template<Type::NonVoid T>
+constexpr int memcmp(T* const& a, T* const& b, usize const& count) {
+	const usize sz = count * sizeof(T);
+	if (sz%8==0) return memcmpX<uint64>(a, b, sz);
+	if (sz%4==0) return memcmpX<uint32>(a, b, sz);
+	if (sz%2==0) return memcmpX<uint16>(a, b, sz);
+	return memcmpX<uint8>((void*)a, (void*)b, sz);
+}
+
+template<Type::Integer I>
+constexpr void* memsetX(void* const& dst, int const& val, usize size) {
+	I* d = dst;
 	while (size-- > 0)
-	*d++ = val;
+		*d++ = val;
 	return dst;
 }
 
+constexpr void* memset(void* const& dst, int const& val, usize size) {
+	return memsetX<uint8>(dst, val, size);
+}
+
 template<Type::NonVoid T>
-constexpr T* memset(T* const& dst, int const& val, usize const& size) {
-	return memset((void*)dst, val, size * sizeof(T));
+constexpr T* memset(T* const& dst, int const& val, usize const& count) {
+	return memset((void*)dst, val, count * sizeof(T));
 }
 
 template<Type::NonVoid T>
@@ -73,14 +105,23 @@ constexpr T* memset(T* const& dst, int const& val) {
 	return memset((void*)dst, val, sizeof(T));
 }
 
-template<typename T>
-constexpr T* memzero(T* const& dst, usize const& size) {
-	return memset((void*)dst, 0, size * sizeof(T));
+template<Type::NonVoid T>
+constexpr T* memzero(void* const& dst, usize const& size) {
+	return memset(dst, 0, size);
 }
 
-template<typename T>
+template<Type::NonVoid T>
+constexpr T* memzero(T* const& dst, usize const& count) {
+	const usize sz = count * sizeof(T);
+	if (sz%8==0) return memsetX<uint64>(dst, 0, sz);
+	if (sz%4==0) return memsetX<uint32>(dst, 0, sz);
+	if (sz%2==0) return memsetX<uint16>(dst, 0, sz);
+	return memsetX<uint8>(dst, 0, sz);
+}
+
+template<Type::NonVoid T>
 constexpr T* memzero(T* const& dst) {
-	return memset((void*)dst, 0, sizeof(T));
+	return memzero((void*)dst, 0, sizeof(T));
 }
 
 #endif // CTL_ALGORITHM_MEMORY_H
