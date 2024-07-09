062f9deb8152b1440758ce0b4f40dcd3740bf04f
LasagnaCake <sponge.cake@tuta.io>
2024-07-08 23:36:08 -0300

    Update obfuscator.hpp

--- src/collection/obfuscator.hpp
+++ src/collection/obfuscator.hpp
@@ -30,14 +30,28 @@ namespace Obfuscation {
 	};
 
 	namespace Impl {
-		template<usize S>
-		struct StringNode;
+		consteval bool isPrime(usize const& v) {
+			for (usize i = 2; i < v; ++i)
+				if (v % i == 0)
+					return false;
+			return true;
+		}
 
-		template<>
-		struct StringNode<1> {
-			constexpr StringNode()								{				}
-			constexpr StringNode(FixedCString<1> const& dat)	{c = dat[0];	}
-			constexpr StringNode(Array<uint8, 1> const& dat)	{c = dat[0];	}
+		consteval usize nearestPrime(usize const& v, bool exclueSelf = false) {
+			for (usize i = (exclueSelf ? (v-1) : v); i > 0; --i)
+				if (nearestPrime(i))
+					return i;
+			return 0;
+		}
+
+		template<usize S, bool PARITY, uint8 MASK>
+		struct StringMangler;
+
+		template<bool PARITY, uint8 MASK>
+		struct StringMangler<1, PARITY, MASK> {
+			constexpr StringMangler()							{				}
+			constexpr StringMangler(FixedCString<1> const& dat)	{c = dat[0];	}
+			constexpr StringMangler(Array<uint8, 1> const& dat)	{c = dat[0];	}
 
 			String build() const {return toString(c);}
 
@@ -45,75 +59,113 @@ namespace Obfuscation {
 			char c;
 		};
 
-		template<usize S>
-		struct StringNode {
-			constexpr StringNode() {}
+		template<usize S, bool PARITY, uint8 MASK>
+		struct StringMangler {
+			constexpr static usize STRING_SIZE = S;
 
-			constexpr StringNode(FixedCString<S> const& dat) {
-				Array<uint8, S1> s1;
-				Array<uint8, S2> s2;
-				for (usize i = 0; i < S2; ++i) {
-					s1[i] = dat[i+S2];
-					s2[i] = dat[i];
-				}
-				if constexpr(S2 < S1)
-					s1[S1-1] = dat[S-1];
-				left = StringNode<S1>(s1);
-				right = StringNode<S2>(s2);
+			constexpr StringMangler() {}
+
+			constexpr StringMangler(FixedCString<STRING_SIZE> const& dat) {
+				decompose<FixedCString<STRING_SIZE>>(dat);
 			}
 
-			constexpr StringNode(Array<uint8, S> const& dat) {
-				Array<uint8, S1> s1;
-				Array<uint8, S2> s2;
-				for (usize i = 0; i < S2; ++i) {
-					s1[i] = dat[i+S2];
-					s2[i] = dat[i];
-				}
-				if constexpr(S2 < S1)
-					s1[S1-1] = dat[S-1];
-				left = StringNode<S1>(s1);
-				right = StringNode<S2>(s2);
+			constexpr StringMangler(Array<uint8, STRING_SIZE> const& dat) {
+				decompose<Array<uint8, STRING_SIZE>>(dat);
 			}
 
-			String build() const {return right.build() + left.build();}
+			String build() const requires (PARITY)	{return right.build() + left.build();}
+			String build() const requires (!PARITY)	{return left.build() + right.build();}
 		private:
-			constexpr static usize S1{(S % 2 == 0) ? (S/2) : (S/2+1)};
-			constexpr static usize S2{(S/2)};
+			template<class TArray>
+			constexpr void decompose(TArray const& dat) {
+				Array<uint8, S1> s1;
+				Array<uint8, S2> s2;
+				for (usize i = 0; i < H2; ++i) {
+					if constexpr (PARITY) {
+						s1[i] = dat[i+H2];
+						s2[i] = dat[i];
+					} else {
+						s1[i] = dat[i];
+						s2[i] = dat[i+H2];
+					}
+				}
+				if constexpr(H2 < H1) {
+					if constexpr (PARITY)
+						s1[H1-1] = dat[STRING_SIZE-1];
+					else
+						s2[H1-1] = dat[STRING_SIZE-1];
+				}
+				left	= StringMangler<S1, LHS_PARITY, NEW_MASK>(s1);
+				right	= StringMangler<S2, RHS_PARITY, NEW_MASK>(s2);
+			}
 
-			StringNode<S1> left;
-			StringNode<S2> right;
+			constexpr static uint8 NEW_MASK = MASK ^ (MASK >> 2);
+
+			constexpr static bool
+				EVEN_SIZE	= (STRING_SIZE % 2 == 0),
+				LHS_PARITY	= (MASK & 0b10) ? !PARITY : PARITY,
+				RHS_PARITY	= (MASK & 0b01) ? !PARITY : PARITY
+			;
+
+			constexpr static usize
+				H1{(EVEN_SIZE) ? (STRING_SIZE/2) : (STRING_SIZE/2+1)},
+				H2{(S/2)}
+			;
+
+			constexpr static usize
+				S1{(PARITY) ? H1 : H2},
+				S2{(PARITY) ? H2 : H1}
+			;
+
+			StringMangler<S1, LHS_PARITY, NEW_MASK> left;
+			StringMangler<S2, RHS_PARITY, NEW_MASK> right;
 		};
 	}
 
-	template <usize N>
+	template<usize S>
+	using StringMangler = Impl::StringMangler<S, true, 0b11000110>;
+
+	template<typename T>
+	concept StringContainer =
+		Type::Constructible<T>
+	&&	Type::Constructible<T, FixedCString<T::STRING_SIZE> const&>
+	&&	requires (T t) {
+			T::STRING_SIZE;
+			{t.build()} -> Type::Equal<String>;
+		}
+	;
+
+	template <usize N, template<usize> class TContainer = StringMangler>
 	struct ObfuscatedString: Obfuscator<String> {
+		static_assert(StringContainer<TContainer<N>>, "Container is not a valid string container!");
+
 		constexpr static usize STRING_SIZE = N+1;
 
-		constexpr ObfuscatedString(FixedCString<STRING_SIZE> const& str): head(decompose(str)) {}
+		constexpr ObfuscatedString(FixedCString<STRING_SIZE> const& str): data(decompose(str)) {}
 
 		constexpr virtual ~ObfuscatedString() {}
 
 		DataType deobfuscated() const final {
 			DataType result;
 			uint8 off = 0;
-			for(uint8 c : head.build())
+			for(uint8 c : data.build())
 				result.push_back(off += c);
 			return result;
 		}
 
 	private:
-		typedef Impl::StringNode<STRING_SIZE> StringWrapper;
+		typedef TContainer<STRING_SIZE> ContainerType;
 
-		StringWrapper head;
+		ContainerType data;
 
-		constexpr static StringWrapper decompose(FixedCString<STRING_SIZE> const& str) {
+		constexpr static ContainerType decompose(FixedCString<STRING_SIZE> const& str) {
 			Array<uint8, STRING_SIZE> result;
 			uint8 off = 0;
 			for (usize i = 0; i < STRING_SIZE; ++i) {
 				result[i] = (str[i] - off);
 				off = str[i];
 			}
-			return StringWrapper(result);
+			return ContainerType(result);
 		};
 	};
 }
