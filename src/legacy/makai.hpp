466116e936941bf6421fac7e6d73c6fb6dead462
LasagnaCake <sponge.cake@tuta.io>
2024-08-17 11:10:18 -0300

    The beginning of things

---
+++ src/legacy/makai.hpp
@@ -0,0 +1,31 @@
+#ifndef MAKAILIB_LIBRARY_HEADER_H
+#define MAKAILIB_LIBRARY_HEADER_H
+
+#pragma GCC diagnostic push
+
+#pragma GCC diagnostic ignored "-Wnarrowing"
+#pragma GCC diagnostic ignored "-Wvla"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#pragma GCC diagnostic ignored "-Wpedantic"
+
+#include "memoverride.hpp"
+
+#include "anchors.hpp"
+#include "audio.hpp"
+#include "program.hpp"
+#include "graphical.hpp"
+#include "gamedata.hpp"
+
+#ifndef MAKAILIB_FORCE_INTEGRATED_GRAPHICS
+extern "C" {
+DLL_EXPORT	uint32	NvOptimusEnablement = 1;
+DLL_EXPORT	int		AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif // MAKAILIB_FORCE_INTEGRATED_GRAPHICS
+
+namespace Makai {
+}
+
+#pragma GCC diagnostic pop
+
+#endif // MAKAILIB_LIBRARY_HEADER_H
