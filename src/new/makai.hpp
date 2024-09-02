89837127b5a9faea485bf449146f3b9bbbb77817
LasagnaCake <80758022+LasagnaCake@users.noreply.github.com>
2024-09-02 19:03:59 -0300

    Create makai.hpp

---
+++ src/new/makai.hpp
@@ -0,0 +1,12 @@
+#ifndef MAKAILIB_H
+#define MAKAILIB_H
+
+#include "audio/audio.hpp"
+#include "core/core.hpp"
+#include "data/data.hpp"
+#include "file/file.hpp"
+#include "graph/graph.hpp"
+#include "audio/audio.hpp"
+#include "audio/audio.hpp"
+
+#endif // MAKAILIB_H
