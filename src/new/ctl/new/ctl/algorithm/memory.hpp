691d4747491491d65735a76ac31a6fa472a5e147
LasagnaCake <sponge.cake@tuta.io>
2024-09-22 11:38:00 -0300

    Update memory.hpp

--- src/new/ctl/new/ctl/algorithm/memory.hpp
+++ src/new/ctl/new/ctl/algorithm/memory.hpp
@@ -21,13 +21,13 @@ namespace MX {
 	typedef uint8* Address;
 
 	constexpr void* memcpy(void* const& dst, const void* const& src, usize size) {
-		#ifdef _DO_NOT_USE_BUILTINS_
+		#ifdef CTL_DO_NOT_USE_BUILTINS
 		Address s = (Address)src, d = (Address)dst;
 		if (size == 1) *d = *s;
 		else while (size--) *d++ = *s++;
 		#else
 		return __builtin_memcpy(dst, src, size);
-		#endif // _DO_NOT_USE_BUILTINS_
+		#endif // CTL_DO_NOT_USE_BUILTINS
 	}
 
 	template<Type::NonVoid T>
@@ -41,7 +41,7 @@ namespace MX {
 	}
 
 	constexpr void* memmove(void* const& dst, const void* const& src, usize size) {
-		#ifdef _DO_NOT_USE_BUILTINS_
+		#ifdef CTL_DO_NOT_USE_BUILTINS
 		Address d = (Address)dst, s = (Address)src;
 		if (d < s)
 			while (size--)
@@ -54,7 +54,7 @@ namespace MX {
 		}
 		#else
 		return __builtin_memmove(dst, src, size);
-		#endif // _DO_NOT_USE_BUILTINS_
+		#endif // CTL_DO_NOT_USE_BUILTINS
 	}
 
 	template<Type::NonVoid T>
@@ -68,14 +68,14 @@ namespace MX {
 	}
 
 	constexpr int memcmp(void* const& a, void* const& b, usize size) {
-		#ifdef _DO_NOT_USE_BUILTINS_
+		#ifdef CTL_DO_NOT_USE_BUILTINS
 		Address s1 = (Address)a, s2 = (Address)b;
 		while (size-- > 0)
 			if (*s1++ != *s2++)
 				return s1[-1] < s2[-1] ? -1 : 1;
 		#else
 		return __builtin_memcmp(a, b, size);
-		#endif // _DO_NOT_USE_BUILTINS_
+		#endif // CTL_DO_NOT_USE_BUILTINS
 	}
 
 	template<Type::NonVoid T>
@@ -84,13 +84,13 @@ namespace MX {
 	}
 
 	constexpr void* memset(void* const& dst, int const& val, usize size) {
-		#ifdef _DO_NOT_USE_BUILTINS_
+		#ifdef CTL_DO_NOT_USE_BUILTINS
 		Address d = (Address)dst;
 		while (size-- > 0)
 			*d++ = val;
 		#else
 		return __builtin_memset(dst, val, size);
-		#endif // _DO_NOT_USE_BUILTINS_
+		#endif // CTL_DO_NOT_USE_BUILTINS
 	}
 
 	template<Type::NonVoid T>
