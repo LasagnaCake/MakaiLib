b9218eb4e8c1c1ad1ab5f9954fdbdb03b6196643
LasagnaCake <sponge.cake@tuta.io>
2024-10-12 23:50:35 -0300

    Update pointer.hpp

--- src/new/ctl/new/ctl/container/pointer.hpp
+++ src/new/ctl/new/ctl/container/pointer.hpp
@@ -137,6 +137,7 @@ public:
 	constexpr bool exists() const {
 		//if (!ref)			return false;
 		CTL_PTR_IF_STRONG	return ref && (database[(void*)ref].count > 0);
+		// BUG: returns true even if exists is set to false
 		else				return ref && (database[(void*)ref].exists);
 	}
 
