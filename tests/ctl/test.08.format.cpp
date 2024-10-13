26a3ef807054596887250d641abfa6918c61a05e
LasagnaCake <sponge.cake@tuta.io>
2024-10-13 11:21:18 -0300

    Update test.08.format.cpp

--- tests/ctl/test.08.format.cpp
+++ tests/ctl/test.08.format.cpp
@@ -7,10 +7,10 @@ void testStringFormatting() {
 	DEBUGLN(CTL::Format::pad("abc", '0', 8, CTL::Format::Justify::CFJ_LEFT));
 	DEBUGLN(CTL::Format::pad("abc", '0', 8, CTL::Format::Justify::CFJ_RIGHT));
 	DEBUGLN(CTL::Format::pad("abcd", '0', 8, CTL::Format::Justify::CFJ_CENTER));
-	DEBUGLN(CTL::Format::prettify(148.3, 4, 4));
-	DEBUGLN(CTL::Format::prettify(148.3, 2, 4));
-	DEBUGLN(CTL::Format::prettify(148.3, 4, 2));
-	DEBUGLN(CTL::Format::prettify(148.3, 2, 2));
+	DEBUGLN(CTL::Format::prettify(148.34, 4, 4));
+	DEBUGLN(CTL::Format::prettify(148.34, 2, 4));
+	DEBUGLN(CTL::Format::prettify(148.34, 4, 2));
+	DEBUGLN(CTL::Format::prettify(148.34, 2, 2));
 }
 
 int main() {
