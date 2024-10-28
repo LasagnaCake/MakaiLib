c6633da6c7b7fa2f3aee74becc7d03b745ff834a
LasagnaCake <sponge.cake@tuta.io>
2024-10-28 17:53:42 -0300

    Update withinbounds.hpp

--- src/new/ctl/ctlex/collision/collision2d/withinbounds.hpp
+++ src/new/ctl/ctlex/collision/collision2d/withinbounds.hpp
@@ -23,7 +23,7 @@ namespace Collision::C2D {
 		;
 	}
 
-	inline bool withinBounds(Vector2 const& point, Box const& area) {
+	constexpr bool withinBounds(Vector2 const& point, Box const& area) {
 		return (
 			( area.min.x < point.x) && (point.x < area.max.x)
 		) && (
@@ -31,7 +31,7 @@ namespace Collision::C2D {
 		);
 	}
 
-	inline bool withinBounds(Vector2 const& point, Circle const& area) {
+	constexpr bool withinBounds(Vector2 const& point, Circle const& area) {
 		// Calculate circle stretch
 		float angle		= point.angleTo(area.position);
 		float radius	= getRadius(area.radius, angle + area.rotation);
@@ -39,7 +39,7 @@ namespace Collision::C2D {
 		return point.distanceTo(area.position) < (radius);
 	}
 
-	inline bool withinBounds(Vector2 const& a, Capsule const& area) {
+	constexpr bool withinBounds(Vector2 const& a, Capsule const& area) {
 		// Get distance between targets
 		float distance = a.distanceTo(area.position);
 		// If too distant, return
@@ -52,17 +52,17 @@ namespace Collision::C2D {
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Vector2 const& a, Ray const& b) {
+	constexpr bool withinBounds(Vector2 const& a, Ray const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Vector2 const& a, Figure const& b) {
+	constexpr bool withinBounds(Vector2 const& a, Figure const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Vector2 const& a, Polygon const& b) {
+	constexpr bool withinBounds(Vector2 const& a, Polygon const& b) {
 		return false;
 	}
 
@@ -70,7 +70,7 @@ namespace Collision::C2D {
 
 	// Circle
 
-	inline bool withinBounds(Circle const& a, Circle const& b) {
+	constexpr bool withinBounds(Circle const& a, Circle const& b) {
 		// Calculate circle stretches
 		float angle		= a.position.angleTo(b.position);
 		float radiusA	= getRadius(a.radius, angle + a.rotation);
@@ -81,7 +81,7 @@ namespace Collision::C2D {
 
 	// Box
 
-	bool withinBounds(Box const& a, Box const& b) {
+	constexpr bool withinBounds(Box const& a, Box const& b) {
 		// Get overlap on X
 		bool overlapX = (
 			(b.min.x < a.min.x) && (a.min.x < b.max.x)
@@ -99,13 +99,13 @@ namespace Collision::C2D {
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	bool withinBounds(Circle const& a, Box const& b) {
+	constexpr bool withinBounds(Circle const& a, Box const& b) {
 		return false;
 	}
 
 	// Capsule
 
-	inline bool withinBounds(Circle const& a, Capsule const& b) {
+	constexpr bool withinBounds(Circle const& a, Capsule const& b) {
 		// Get distance between targets
 		float distance = a.position.distanceTo(b.position);
 		// If too distant, return
@@ -118,11 +118,11 @@ namespace Collision::C2D {
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Box const& a, Capsule const& b) {
+	constexpr bool withinBounds(Box const& a, Capsule const& b) {
 		return false;
 	}
 
-	inline bool withinBounds(Capsule const& a, Capsule const& b) {
+	constexpr bool withinBounds(Capsule const& a, Capsule const& b) {
 		// Get distance between targets
 		float distance = a.position.distanceTo(b.position);
 		// If too distant, return
@@ -137,113 +137,113 @@ namespace Collision::C2D {
 	// Ray
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Box const& a, Ray const& b) {
+	constexpr bool withinBounds(Box const& a, Ray const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Circle const& a, Ray const& b) {
+	constexpr bool withinBounds(Circle const& a, Ray const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Capsule const& a, Ray const& b) {
+	constexpr bool withinBounds(Capsule const& a, Ray const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Ray const& a, Ray const& b) {
+	constexpr bool withinBounds(Ray const& a, Ray const& b) {
 		return false;
 	}
 
 	// Shape
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Box const& a, Figure const& b) {
+	constexpr bool withinBounds(Box const& a, Figure const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Circle const& a, Figure const& b) {
+	constexpr bool withinBounds(Circle const& a, Figure const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Capsule const& a, Figure const& b) {
+	constexpr bool withinBounds(Capsule const& a, Figure const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Ray const& a, Figure const& b) {
+	constexpr bool withinBounds(Ray const& a, Figure const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Figure const& a, Figure const& b) {
+	constexpr bool withinBounds(Figure const& a, Figure const& b) {
 		return false;
 	}
 
 	// Polygon
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Box const& a, Polygon const& b) {
+	constexpr bool withinBounds(Box const& a, Polygon const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Circle const& a, Polygon const& b) {
+	constexpr bool withinBounds(Circle const& a, Polygon const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Capsule const& a, Polygon const& b) {
+	constexpr bool withinBounds(Capsule const& a, Polygon const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Ray const& a, Polygon const& b) {
+	constexpr bool withinBounds(Ray const& a, Polygon const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Figure const& a, Polygon const& b) {
+	constexpr bool withinBounds(Figure const& a, Polygon const& b) {
 		return false;
 	}
 
 	[[gnu::warning("Unimplemented!")]]
-	inline bool withinBounds(Polygon const& a, Polygon const& b) {
+	constexpr bool withinBounds(Polygon const& a, Polygon const& b) {
 		return false;
 	}
 
 	// Flipped Functions
 
 	template<Collidable T>
-	inline bool withinBounds(Circle const& a, T const& b) {
+	constexpr bool withinBounds(Circle const& a, T const& b) {
 		return withinBounds(b, a);
 	}
 
 	template<Collidable T>
-	inline bool withinBounds(Box const& a, T const& b) {
+	constexpr bool withinBounds(Box const& a, T const& b) {
 		return withinBounds(b, a);
 	}
 
 	template<Collidable T>
-	inline bool withinBounds(Capsule const& a, T const& b) {
+	constexpr bool withinBounds(Capsule const& a, T const& b) {
 		return withinBounds(b, a);
 	}
 
 	template<Collidable T>
-	inline bool withinBounds(Ray const& a, T const& b) {
+	constexpr bool withinBounds(Ray const& a, T const& b) {
 		return withinBounds(b, a);
 	}
 
 	template<Collidable B>
-	inline bool withinBounds(Figure const& a, B const& b) {
+	constexpr bool withinBounds(Figure const& a, B const& b) {
 		return withinBounds(b, a);
 	}
 
 	template<Collidable T>
-	inline bool withinBounds(Polygon const& a, T const& b) {
+	constexpr bool withinBounds(Polygon const& a, T const& b) {
 		return withinBounds(b, a);
 	}
 
