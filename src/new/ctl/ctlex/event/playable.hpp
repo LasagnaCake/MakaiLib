5e4164e9ed26c92da4d9ef067259d8af5f6d94e6
LasagnaCake <sponge.cake@tuta.io>
2024-11-27 15:24:23 -0300

    Update playable.hpp

--- src/new/ctl/ctlex/event/playable.hpp
+++ src/new/ctl/ctlex/event/playable.hpp
@@ -7,17 +7,30 @@ CTL_EX_NAMESPACE_BEGIN
 
 /// @brief Controllable event interface.
 struct Playable {
+	/// @brief Starts the event.
+	/// @return Reference to self.
 	virtual Playable&	start()		= 0;
+	/// @brief Resumes the event.
+	/// @return Reference to self.
 	virtual Playable&	play()		= 0;
+	/// @brief Pauses the event.
+	/// @return Reference to self.
 	virtual Playable&	pause()		= 0;
+	/// @brief Stops the event.
+	/// @return Reference to self.
 	virtual Playable&	stop()		= 0;
+	/// @brief Returns whether the event is finished being processed.
+	/// @return Whether the event is finished being processed.
 	virtual bool		finished()	{return isFinished;	}
 
+	/// @brief Whether the event is paused.
 	bool paused		= false;
 
+	/// @brief Virtual destructor.
 	virtual ~Playable() {}
 
 protected:
+	/// @brief Whether the event is finished being processed.
 	bool isFinished	= false;
 };
 
