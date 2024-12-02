78747e984e7426a332a6b1c111775922f9a36b97
LasagnaCake <sponge.cake@tuta.io>
2024-12-02 20:30:07 -0300

    Update test.03.graphics.cpp

--- tests/makai/test.03.graphics.cpp
+++ tests/makai/test.03.graphics.cpp
@@ -21,10 +21,11 @@ struct TestApp: Makai::App {
 
 	TestApp(): Makai::App(600, 400, "Test 03", false) {
 		DEBUGLN("Loading shaders...");
-		loadShaders(
+		/*loadShaders(
 			Makai::File::getSLF("shaders/base/base.slf"),
 			Makai::File::getSLF("shaders/framebuffer/compose.slf")
-		);
+		);*/
+		loadDefaultShaders();
 		DEBUGLN("Creating cube...");
 		cube.material.culling = Makai::Graph::CullMode::OCM_FRONT;
 		//cube.material.shaded = true;
